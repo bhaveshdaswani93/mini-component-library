6422285c25a6f792441651af18211338edb981f5
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2023-01-16 13:25:36 +0530

    solution for progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants"
+import VisuallyHidden from "../VisuallyHidden"
+
+const Progress = styled.progress`
+  padding: var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  accent-color: ${COLORS.primary};
+
+`
+
+const SIZES = {
+  small: {
+    '--padding': '4px 0'
+  },
+
+  medium: {
+    '--padding': '8px 0'
+  },
+
+  large: {
+    '--padding': '12px 0'
+  }
+}
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+  let overRideValue = value;
 
-export default ProgressBar;
+  if (overRideValue < 0) {
+    overRideValue = 0;
+  }
+
+  if (overRideValue > 100) {
+    overRideValue = 100;
+  }
+
+
+
+  const styles = SIZES[size];
+  return (
+    <>
+      <Progress
+        aria-valuenow={overRideValue}
+        value={overRideValue}
+        role="progressbar"
+        aria-valuemin={0}
+        aria-valuemax={100}
+        min={0}
+        max={100}
+        style={styles}
+      >{overRideValue}</Progress>
+      <VisuallyHidden>Progress Bar</VisuallyHidden>
+    </>
+  )
+}
+
+export default ProgressBar
