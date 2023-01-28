a38932aeda4db82732a83c7b6883899992d46d93
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2023-01-28 17:13:50 +0530

    working on select layout

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,59 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const SelectInput = styled.select`
+position: absolute;
+left: 0;
+right: 0;
+top: 0;
+bottom: 0;
+opacity: 0;
+&:focus {
+    
+  }
+`;
+
+const SelectContainer = styled.div`
+  position: relative;
+  width: max-content;
+  
+`;
+
+const PresentationalSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  font-size: 16px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  color: ${COLORS.gray500};
+  ${SelectInput}:hover + & {
+    color: ${COLORS.black};
+  }
+  ${SelectInput}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectContainer>
+     
+      <SelectInput value={value} onChange={onChange}>
+        {children}
+      </SelectInput>
+      <PresentationalSelect>
+        <span>{displayedValue}</span>
+        <Icon size='24px' id='chevron-down' />
+      </PresentationalSelect>
+    </SelectContainer>
+    
   );
 };
 
